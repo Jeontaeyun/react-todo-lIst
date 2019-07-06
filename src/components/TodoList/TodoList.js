49cb052c4e9a10b75dc8d17ffe610ebd58ce5f0e
Jeontaeyun <https://github.com/Jeontaeyun>
2019-07-06 18:31:30 +1000

    01. 컴포넌트 최적화 및 README.md 파일 작성

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,6 +2,12 @@ import React, {Component} from 'react';
 import TodoItem from '../TodoItem';
 
 class TodoList extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.todos !== nextProps.todos;
+        /*todos의 값이ㅣ 바뀔 때만 업데이트를 한다.*/
+    };
+
     render() {
         const {todos, onToggle, onRemove} = this.props;
         const todoList = todos.map((todo) => (<TodoItem key={todo.id} done={todo.done} onToggle={()=>onToggle(todo.id)} onRemove={()=>onRemove(todo.id)}>{todo.text}</TodoItem>));
